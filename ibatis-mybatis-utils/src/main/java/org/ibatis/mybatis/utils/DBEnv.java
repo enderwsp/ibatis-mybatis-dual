c7a49a3329964d8b0cba02e98a15659fc4983ac5
vic <499636487@qq.com>
2024-03-01 17:32:33 +0800

    ibatis only aspectj
    ibatis only aspectj

--- ibatis-mybatis-utils/src/main/java/org/ibatis/mybatis/utils/DBEnv.java
+++ ibatis-mybatis-utils/src/main/java/org/ibatis/mybatis/utils/DBEnv.java
@@ -2,16 +2,18 @@ package org.ibatis.mybatis.utils;
 
 public class DBEnv {
     public static class Master{
-        public static String Driver="org.h2.Driver";
+        public static String Driver=org.h2.Driver.class.getName();
+//        public static String Driver="org.h2.Driver";
         public static String Url="jdbc:h2:~/test";
         public static String UserName="sa";
         public static String Password="";
     }
     public static class SlavesOne{
-        public static String Driver="";
-        public static String Url="";
-        public static String UserName="";
-        public static String Password="";
+        public static String Driver=com.mysql.cj.jdbc.Driver.class.getName();
+//        public static String Driver="com.mysql.cj.jdbc.Driver";
+        public static String Url="jdbc:mysql://127.0.0.1:3306/hzj?characterEncoding=UTF-8&useUnicode=true&useSSL=false&tinyInt1isBit=false&rewriteBatchedStatements=true&serverTimezone=Asia/Shanghai";
+        public static String UserName="root";
+        public static String Password="root";
     }
     public static class SlavesTwo{
         public static String Driver="";
--- ibatis-noSpring-sample/src/main/java/org/example/IbatisTest.java
+++ ibatis-noSpring-sample/src/main/java/org/example/IbatisTest.java
@@ -12,7 +12,6 @@ import com.ibatis.sqlmap.engine.transaction.jdbc.JdbcTransactionConfig;
 import org.ibatis.mybatis.utils.DBEnv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -23,17 +22,21 @@ import java.util.Map;
 
 public class IbatisTest {
     public static Logger log = LoggerFactory.getLogger(IbatisTest.class);
+    public static String resource = "ibatis/SqlMapConfig.xml";
+
 
     public static void main(String[] args) {
-        log = LoggerFactory.getLogger(IbatisTest.class);
-        System.setProperty("log4j.configurationFile", "log4j2.xml");
-        log.atLevel(Level.INFO);
-        String resource = "ibatis/SqlMapConfig.xml";
+        SqlMapClientImpl sqlMapClient = init(resource, DBEnv.Master.Driver, DBEnv.Master.Url, DBEnv.Master.UserName, DBEnv.Master.Password);
+
+        exeSql(sqlMapClient, true);
+    }
+
+    public static SqlMapClientImpl init(String resource, String drver, String url, String name, String pw) {
         Map ps = new HashMap();
-        ps.put("JDBC.Driver", DBEnv.Master.Driver);
-        ps.put("JDBC.ConnectionURL", DBEnv.Master.Url);
-        ps.put("JDBC.Username", DBEnv.Master.UserName);
-        ps.put("JDBC.Password", DBEnv.Master.Password);
+        ps.put("JDBC.Driver", drver);
+        ps.put("JDBC.ConnectionURL", url);
+        ps.put("JDBC.Username", name);
+        ps.put("JDBC.Password", pw);
         SimpleDataSource dataSource = new SimpleDataSource(ps);
         SqlMapClientImpl sqlMapClient;
         try {
@@ -45,39 +48,37 @@ public class IbatisTest {
         txc.setDataSource(dataSource);
         TransactionManager txm = new TransactionManager(txc);
         sqlMapClient.getDelegate().setTxManager(txm);
-        exeSql(sqlMapClient, true);
+        return sqlMapClient;
     }
 
     private static void exeSql(SqlMapClient sqlMapClient, boolean useTx) {
-        SqlMapSession session = null;
-        Connection con = null;
         try {
-            session = sqlMapClient.openSession();
+
             if (useTx) {
-                session.startTransaction();
+                sqlMapClient.startTransaction();
             }
             Map dd = new HashMap();
-            dd.put("id", 1234);
+            dd.put("id", 1236);
             dd.put("username", "TestIbatis");
             dd.put("usernamelike", "Test%");
             dd.put("password", System.currentTimeMillis() + "-PW");
             dd.put("email", System.currentTimeMillis() + "@123.com");
-            session.insert("testibatis.insert", dd);
-            int udx = session.update("testibatis.update", dd);
+            sqlMapClient.insert("testibatis.insert", dd);
+            int udx = sqlMapClient.update("testibatis.update", dd);
             log.info("----------update:udx:" + udx);
-            List<Map> rs = session.queryForList("testibatis.select", dd);
+            List<Map> rs = sqlMapClient.queryForList("testibatis.select", dd);
             log.info("----------select:rs:" + rs);
-            int dudx = session.delete("testibatis.delete", dd);
+            int dudx = sqlMapClient.delete("testibatis.delete", dd);
             log.info("----------delete:dudx:" + dudx);
             if (useTx) {
-                session.commitTransaction();
+                sqlMapClient.commitTransaction();
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
             if (useTx) {
                 try {
-                    session.endTransaction();
+                    sqlMapClient.endTransaction();
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
